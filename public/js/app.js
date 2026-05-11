056bcf164c8b529d9c060dc02b8fc11563377453
Jose Mesa <josemesaviaje@gmail.com>
2026-05-11 12:46:49 +0200

    final production api base

--- public/js/app.js
+++ public/js/app.js
@@ -17,7 +17,11 @@
   const EXPENSE_PIN = "2026";
   let expensesUnlocked = false;
   
-  const API_BASE = "http://127.0.0.1:3050";
+const API_BASE =
+  window.location.hostname === "127.0.0.1" ||
+  window.location.hostname === "localhost"
+    ? "http://127.0.0.1:3050"
+    : window.location.origin;
 
 let lastServerUpdatedAt = null;
 let socket = null;
